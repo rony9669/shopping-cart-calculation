a9129f75f4f302c7f687352395f1ee889dc837e2
rony9669 <ronychowdhury.cse@gmail.com>
2022-09-28 23:47:56 +0600

    card section added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,32 +1,27 @@
 import React from "react";
 import "./Product.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Product = (props) => {
   const { handleAddToCartClick } = props;
-  const { name, img, seller, price, ratings, id } = props.product;
+  const { name, img, description, time, id } = props.product;
 
   //   console.log(props.product);
 
   return (
     <div className="product">
-      <img
-        src={img ? img : <img src="../../images/no image.jpg"></img>}
-        alt=""
-      />
+      <img src={img} alt="" />
       <div className="product-details">
-        <h4>Product Name: {name}</h4>
-        <h3 className="price">Price: ${price}</h3>
-        <h5>Seller: {seller}</h5>
-        <h5>Ratings: {ratings}</h5>
+        <h3>Book Name: {name}</h3>
+        <p>
+          <small>{description}</small>
+        </p>
+        <h3 className="price">Time in minutes:{time}</h3>
       </div>
       <button
         onClick={() => handleAddToCartClick(props.product)}
         className="btn-cart"
       >
-        <p>Add to Cart</p>
-        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+        <p>Add to List</p>
       </button>
     </div>
   );
